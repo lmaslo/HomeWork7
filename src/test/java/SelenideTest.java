93ffd285f1de4b816a5174130fcc8ee01561798d
lmaslo <PerepechkinaEV@yandex.ru>
2022-02-26 17:59:13 +0300

    new java class

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    public void testIssueSearch(){
+        open("https://github.com/");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(By.linkText("eroshenkoam/allure-example")).click();
+        $(By.partialLinkText("Issues")).click();
+        $(withText("68")).should(Condition.exist);
+
+
+    }
+}
